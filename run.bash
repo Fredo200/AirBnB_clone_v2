228626aa004e33cbb03ef7a2141ed2fe2c80fd60
Fredo200 <fredoduor293@gmail.com>
2024-06-25 04:53:36 -0700

    AirBnB

--- run.bash
+++ run.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# Initialize environment variables
 HBNB_ENV=''
 HBNB_TYPE_STORAGE=''
 HBNB_MYSQL_USER=''
@@ -8,68 +10,68 @@ HBNB_MYSQL_PWD=''
 APP_FILE=''
 APP_ARGS=()
 
-# get the application or command to run
-if [[ ${#@} -gt 0 ]]; then
-    file="$1"
-    if [ -x "$file" ]; then
-        start_str=$(echo "$file" | cut -c -2)
-        if [[ "$start_str" != "./" ]]; then
-            APP_FILE='./'"$file"
-        else
-            APP_FILE="$file"
-        fi
-    else
-        APP_FILE=$(echo "$file" | cut -d ' ' -f 1)
-        args_str=$(echo "$file" | cut -d ' ' -f 2-)
-        read -sr -a APP_ARGS < <(echo "$args_str")
-    fi
-else
+# Function to print usage and exit
+print_usage_and_exit() {
     echo -e "\e[31mError:\e[0m No file or command provided"
     echo 'Usage: ./run.bash file|command [environment] [storage]'
     exit 1
+}
+
+# Get the application or command to run
+if [[ $# -gt 0 ]]; then
+    file="$1"
+    if [ -x "$file" ]; then
+        APP_FILE="$file"
+        [[ "${APP_FILE:0:2}" != "./" ]] && APP_FILE="./$file"
+    else
+        APP_FILE=$(echo "$file" | cut -d ' ' -f 1)
+        args_str=$(echo "$file" | cut -d ' ' -f 2-)
+        read -r -a APP_ARGS <<< "$args_str"
+    fi
+else
+    print_usage_and_exit
 fi
 
-# get the application environment
-if [[ ${#@} -gt 1 ]]; then
+# Get the application environment
+if [[ $# -gt 1 ]]; then
     HBNB_ENV="$2"
 else
     read -p 'Environment [dev]: ' -r HBNB_ENV
-    if [[ "$HBNB_ENV" == '' ]]; then
-        HBNB_ENV='dev'
-    fi
+    HBNB_ENV=${HBNB_ENV:-dev}
 fi
 
-# get the storage mechanism
-if [[ ${#@} -gt 2 ]]; then
+# Get the storage mechanism
+if [[ $# -gt 2 ]]; then
     HBNB_TYPE_STORAGE="$3"
 else
     read -p 'Storage Type [db]: ' -r HBNB_TYPE_STORAGE
-    if [[ "$HBNB_TYPE_STORAGE" == '' ]]; then
-        HBNB_TYPE_STORAGE='db'
-    fi
-fi
-if [[ "$HBNB_TYPE_STORAGE" == 'db' ]]; then
-    read -p 'User [hbnb_dev]: ' -r HBNB_MYSQL_USER
-    if [[ "$HBNB_MYSQL_USER" == '' ]]; then
-        HBNB_MYSQL_USER='hbnb_dev'
-    fi
-    read -p 'Host [localhost]: ' -r HBNB_MYSQL_HOST
-    if [[ "$HBNB_MYSQL_HOST" == '' ]]; then
-        HBNB_MYSQL_HOST='localhost'
-    fi
-    read -p 'Database [hbnb_dev_db]: ' -r HBNB_MYSQL_DB
-    if [[ "$HBNB_MYSQL_DB" == '' ]]; then
-        HBNB_MYSQL_DB='hbnb_dev_db'
-    fi
-    read -p 'Enter DB password: ' -sr HBNB_MYSQL_PWD
+    HBNB_TYPE_STORAGE=${HBNB_TYPE_STORAGE:-db}
 fi
 
+# Get database details if storage type is 'db'
+if [[ "$HBNB_TYPE_STORAGE" == 'db' ]]; then
+    read -p 'User [hbnb_dev]: ' -r HBNB_MYSQL_USER
+    HBNB_MYSQL_USER=${HBNB_MYSQL_USER:-hbnb_dev}
+
+    read -p 'Host [localhost]: ' -r HBNB_MYSQL_HOST
+    HBNB_MYSQL_HOST=${HBNB_MYSQL_HOST:-localhost}
+
+    read -p 'Database [hbnb_dev_db]: ' -r HBNB_MYSQL_DB
+    HBNB_MYSQL_DB=${HBNB_MYSQL_DB:-hbnb_dev_db}
+
+    read -sp 'Enter DB password: ' HBNB_MYSQL_PWD
+    echo
+fi
+
+# Echo the command being run
 echo -e "Running \e[34m[$APP_FILE]\e[0m"
-# shellcheck disable=SC2086
+
+# Run the application with the environment variables
 env HBNB_MYSQL_USER="$HBNB_MYSQL_USER" \
-  HBNB_MYSQL_HOST="$HBNB_MYSQL_HOST" \
-  HBNB_MYSQL_DB="$HBNB_MYSQL_DB" \
-  HBNB_ENV="$HBNB_ENV" \
-  HBNB_TYPE_STORAGE="$HBNB_TYPE_STORAGE" \
-  HBNB_MYSQL_PWD="$HBNB_MYSQL_PWD" \
-  "$APP_FILE" ${APP_ARGS[*]}
+    HBNB_MYSQL_HOST="$HBNB_MYSQL_HOST" \
+    HBNB_MYSQL_DB="$HBNB_MYSQL_DB" \
+    HBNB_ENV="$HBNB_ENV" \
+    HBNB_TYPE_STORAGE="$HBNB_TYPE_STORAGE" \
+    HBNB_MYSQL_PWD="$HBNB_MYSQL_PWD" \
+    "$APP_FILE" "${APP_ARGS[@]}"
+
--- tests.bash
+++ tests.bash
@@ -1,40 +1,68 @@
 #!/usr/bin/env bash
+
 # Requirements
 # pip install pycodestyle==2.7.0
 # pip install pep8==1.7.0
 
-## Code tests
-# Runs the unit tests for this project in both the
-# interactive and non-interactive modes
-echo -e "\e[104m Running Unit Tests [FileStorage] \e[0m\e[33m"
-env HBNB_MYSQL_USER="" \
-    HBNB_MYSQL_HOST="" \
-    HBNB_MYSQL_DB="" \
-    HBNB_ENV="test" \
-    HBNB_TYPE_STORAGE="file" \
-    HBNB_MYSQL_PWD="" \
-    'python3' '-m' 'unittest' 'discover' 'tests' \
-&& echo -e "\e[0m\e[104m Running Unit Tests [DBStorage] \e[0m\e[33m" \
-&& env HBNB_MYSQL_USER="hbnb_test" \
-    HBNB_MYSQL_HOST="localhost" \
-    HBNB_MYSQL_DB="hbnb_test_db" \
-    HBNB_ENV="test" \
-    HBNB_TYPE_STORAGE="db" \
-    HBNB_MYSQL_PWD="hbnb_test_pwd" \
-    'python3' '-m' 'unittest' 'discover' 'tests'
+# Function to run unit tests with different configurations
+run_unit_tests() {
+    local storage_type=$1
+    echo -e "\e[104m Running Unit Tests [${storage_type}] \e[0m\e[33m"
+    
+    if [[ $storage_type == "FileStorage" ]]; then
+        env HBNB_MYSQL_USER="" \
+            HBNB_MYSQL_HOST="" \
+            HBNB_MYSQL_DB="" \
+            HBNB_ENV="test" \
+            HBNB_TYPE_STORAGE="file" \
+            HBNB_MYSQL_PWD="" \
+            python3 -m unittest discover tests
+    else
+        env HBNB_MYSQL_USER="hbnb_test" \
+            HBNB_MYSQL_HOST="localhost" \
+            HBNB_MYSQL_DB="hbnb_test_db" \
+            HBNB_ENV="test" \
+            HBNB_TYPE_STORAGE="db" \
+            HBNB_MYSQL_PWD="hbnb_test_pwd" \
+            python3 -m unittest discover tests
+    fi
+    
+    if [[ $? -ne 0 ]]; then
+        echo -e "\e[0m\e[101m Unit Tests Failed [${storage_type}] \e[0m"
+        exit 1
+    fi
+    
+    echo -e "\e[0m"
+}
 
-# echo "python3 -m unittest discover tests" | bash
-[ "$(echo -n $?)" == "0" ] && echo -ne "\e[100m\e[32m PASSED "
-echo -e "\e[0m"
+# Run unit tests
+run_unit_tests "FileStorage"
+run_unit_tests "DBStorage"
+echo -e "\e[100m\e[32m PASSED \e[0m"
 
-## Python code style checks
-echo -e "\e[104m Running Style Checks \e[0m\e[31m"
-Src_Files="$(find . -type f -regex '.*.py' | tr '\n' ' ')"
-# shellcheck disable=SC2086
-pycodestyle $Src_Files
-if [[ "$(($? + 0))" == "0" ]]; then
-    # shellcheck disable=SC2086
-    ~/.local/bin/pep8 $Src_Files
-    [ "$(echo -n $?)" == "0" ] && echo -ne "\e[100m\e[32m PASSED "
-fi
-echo -ne "\n\e[0m"
+# Function to run style checks
+run_style_checks() {
+    echo -e "\e[104m Running Style Checks \e[0m\e[31m"
+    
+    Src_Files=$(find . -type f -name '*.py' | tr '\n' ' ')
+    
+    pycodestyle $Src_Files
+    if [[ $? -eq 0 ]]; then
+        ~/.local/bin/pep8 $Src_Files
+        if [[ $? -eq 0 ]]; then
+            echo -e "\e[100m\e[32m PASSED \e[0m"
+        else
+            echo -e "\e[0m\e[101m PEP8 Checks Failed \e[0m"
+            exit 1
+        fi
+    else
+        echo -e "\e[0m\e[101m PyCodeStyle Checks Failed \e[0m"
+        exit 1
+    fi
+    
+    echo -e "\e[0m"
+}
+
+# Run style checks
+run_style_checks
+
